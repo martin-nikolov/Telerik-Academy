04d62ce41f478b925ce66bfcb99b02e56321a615
Martin Nikolov <flextry@gmail.com>
2013-04-29 23:18:08 +0300

    Update Program.cs

--- Programming/1.CSharpPartOne/01.Introduction to Programming with C-Sharp/9. The first ten members of sequence/Program.cs
+++ Programming/1.CSharpPartOne/01.Introduction to Programming with C-Sharp/9. The first ten members of sequence/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace _9.The_first_ten_members_of_sequence
 {

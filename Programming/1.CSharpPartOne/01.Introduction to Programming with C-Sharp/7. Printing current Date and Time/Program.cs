01e0e415234dba23ba35d03f27aa96aafb974167
Martin Nikolov <flextry@gmail.com>
2013-04-29 23:17:59 +0300

    Update Program.cs

--- Programming/1.CSharpPartOne/01.Introduction to Programming with C-Sharp/7. Printing current Date and Time/Program.cs
+++ Programming/1.CSharpPartOne/01.Introduction to Programming with C-Sharp/7. Printing current Date and Time/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace _7.Printing_current_Date_and_Time
 {

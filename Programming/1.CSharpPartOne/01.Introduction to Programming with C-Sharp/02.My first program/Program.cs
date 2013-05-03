51173598406f43c1fb86a84007ea0a0867f8403f
Martin Nikolov <martin.nikolov96@gmail.com>
2013-05-03 13:30:29 +0300

    Update

---
+++ Programming/1.CSharpPartOne/01.Introduction to Programming with C-Sharp/02.My first program/Program.cs
@@ -0,0 +1,11 @@
+﻿// 2. Create, compile and run a “Hello C#” console application.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello C#!");
+    }
+}
---
+++ Programming/1.CSharpPartOne/01.Introduction to Programming with C-Sharp/09.The first ten members of sequence/Program.cs
@@ -0,0 +1,15 @@
+﻿// 9. Write a program that prints the first 10 members
+// of the sequence: 2, -3, 4, -5, 6, -7, ...
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 2; i <= 12; i++)
+        {
+            Console.WriteLine(i % 2 == 0 ? i : -i);
+        }
+    }
+}

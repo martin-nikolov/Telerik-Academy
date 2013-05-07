50fcb07fe076a4d7a1d9e641afeb46be8c0eeded
Martin Nikolov <martin.nikolov96@gmail.com>
2013-05-07 19:01:14 +0300

    Upload

---
+++ Programming/1. C# Fundamentals I/02. Primitive Data Types and Variables/04. Integer in Hexadecimal format/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+* 4. Declare an integer variable and assign it with the value 254
+* in hexadecimal format. Use Windows Calculator to find its
+* hexadecimal representation.
+*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int hex = 0x254; // 596 in decimal
+        Console.WriteLine("Hexadecimal number {0:X} in decimal format {0}.", hex);
+    }
+}
---
+++ Programming/1. C# Fundamentals I/02. Primitive Data Types and Variables/09. Isosceles symbol triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+* 9. Write a program that prints an isosceles triangle of 9 copyright symbols ©.
+* Use Windows Character Map to find the Unicode code of the © symbol.
+* Note: the © symbol may be displayed incorrectly.
+*/
+
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        int thighLength = 3;
+        char specialSymbol = '©';
+        //Console.Write("Enter length of triangle's thigh: ");
+        //thighLength = int.Parse(Console.ReadLine());
+
+        int cols = thighLength * 2 - 1; // length of hypotenuse
+        int symbolOnRow = 1; // increasing with 2 on every row
+
+        Console.WriteLine("Isosceles triangle built with the symbol " + specialSymbol + ":\n");
+
+        for (int col = 1; col <= thighLength; col++)
+        {
+            int blankSpaces = cols - symbolOnRow / 2;
+
+            Console.Write(new string(' ',blankSpaces));
+            Console.WriteLine(new string(specialSymbol,symbolOnRow));
+
+            symbolOnRow = symbolOnRow + 2;
+        }
+
+        Console.WriteLine();
+    }
+}

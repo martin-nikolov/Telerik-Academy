5d2599628989e0a843a33a9059969662b5d4cc62
Martin Nikolov <flextry@gmail.com>
2013-05-07 20:33:58 +0300

    Update Program.cs

--- Programming/1. C# Fundamentals I/02. Primitive Data Types and Variables/04. Integer in Hexadecimal format/Program.cs
+++ Programming/1. C# Fundamentals I/02. Primitive Data Types and Variables/04. Integer in Hexadecimal format/Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 * 4. Declare an integer variable and assign it with the value 254
 * in hexadecimal format. Use Windows Calculator to find its
 * hexadecimal representation.
@@ -10,7 +10,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        int hex = 0x254; // 596 in decimal
+        int hex = 0xFE; // 254 in decimal
         Console.WriteLine("Hexadecimal number {0:X} in decimal format {0}.", hex);
     }
 }

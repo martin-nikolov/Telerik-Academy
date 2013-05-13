87ebe9ee4ed4ab148b806cab84460f97cbb3f696
Martin Nikolov <martin.nikolov96@gmail.com>
2013-05-13 21:44:06 +0300

    Update

--- Programming/1. C# Fundamentals I/02. Primitive Data Types and Variables/06. IsFemale/isFemale.cs
+++ Programming/1. C# Fundamentals I/02. Primitive Data Types and Variables/06. IsFemale/isFemale.cs
@@ -4,12 +4,25 @@
 */
 
 using System;
-
-class isFemale
+ 
+class IsFemale
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        bool isFemale = false;
-        Console.WriteLine("My gender is female: {0}", isFemale);
+        Console.Write("Enter your gender (Male/Female): ");
+        string gender = Console.ReadLine();
+
+        switch (gender.ToLower())
+        {
+            case "female":
+                Console.WriteLine("Yes, you are female!");
+                break;
+            case "male":
+                Console.WriteLine("No, you are male!");
+                break;
+            default:
+                Console.WriteLine("Incorrect input data!");
+                break;
+        }
     }
 }

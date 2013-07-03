5abac67b755013df96b984243de61c17a8aeff64
flextry <flextry@Gmail.com>
2013-07-03 17:13:08 +0300

    Update

---
+++ Programming with C#/1. C# Fundamentals I/02. Primitive Data Types and Variables/07. Type casting test/TypeCastingTest.cs
@@ -0,0 +1,23 @@
+﻿/*
+* 7. Declare two string variables and assign them with “Hello” and “World”.
+* Declare an object variable and assign it with the concatenation of
+* the first two variables (mind adding an interval). 
+* Declare a third string variable and initialize it with the
+* value of the object variable (you should perform type casting).
+*/
+
+using System;
+
+class TypeCastingTest
+{
+    static void Main(string[] args)
+    {
+        string first = "Hello";
+        string second = "World!";
+
+        object concat = first + " " + second; // or string.Concat(first, " ", second)
+        string result = concat.ToString(); // or (string)concat
+
+        Console.WriteLine(result);
+    }
+}

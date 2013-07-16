1fbc548d57136bd4a8b6cd3249164591dbaf03e0
Martin Nikolov <martin.nikolov96@gmail.com>
2013-07-16 21:29:33 +0300

    Update

--- Programming with C#/Codes/Sort/MergeSort.cs
+++ Programming with C#/Codes/Sort/MergeSort.cs
@@ -12,30 +12,31 @@ class MergeSortAlgorithm
         int[] unsortedNumbers = { 1, -2, 3, -4, 5, -6, 7, -8, 9, -10 };
         int[] tempNums = new int[unsortedNumbers.Length];
 
-        PrintElements(unsortedNumbers); Console.Write(" -> ");
+        Console.Write(string.Join(" ", unsortedNumbers) + " -> "); 
         MergeSort(unsortedNumbers, tempNums, 0, unsortedNumbers.Length - 1);
-        PrintElements(unsortedNumbers); Console.Write("\n");
+        Console.WriteLine(string.Join(" ", unsortedNumbers));
+
 
         string[] unsortedSymbols = new string[] { "b", "d", "c", "a", "f", "w", "z" };
         string[] tempSymb = new string[unsortedNumbers.Length];
 
-        PrintElements(unsortedSymbols); Console.Write(" -> ");
+        Console.Write(string.Join(" ", unsortedSymbols) + " -> ");
         MergeSort(unsortedSymbols, tempSymb, 0, unsortedSymbols.Length - 1);
-        PrintElements(unsortedSymbols); Console.Write("\n");
-        
+        Console.WriteLine(string.Join(" ", unsortedSymbols));
+
+
         char[] unsortedLetters = { 'z', 'b', 'd', 'c', 'w', 'a', 'f' };
         char[] tempLett = new char[unsortedNumbers.Length];
 
-        PrintElements(unsortedLetters); Console.Write(" -> ");
+        Console.Write(string.Join(" ", unsortedLetters) + " -> ");
         MergeSort(unsortedLetters, tempLett, 0, unsortedLetters.Length - 1);
-        PrintElements(unsortedLetters); Console.Write("\n");
+        Console.WriteLine(string.Join(" ", unsortedLetters));
     }
 
     static void MergeSort<T>(T[] unsortedArr, T[] tempArr, int left, int right) where T : IComparable<T>
     {
         // Array with 1 element
-        if (left >= right)
-            return;
+        if (left >= right) return;
 
         // Define a middle of the array
         int middle = left + (right - left) / 2;
@@ -48,29 +49,18 @@ class MergeSortAlgorithm
 
     static void CompareAndSort<T>(T[] arr, T[] tempArr, int left, int middle, int right) where T : IComparable<T>
     {
-        int leftIndexArr = left, middleIndexArr = middle + 1, leftTemp = left;
+        int i = left; // 'temp' indexes
+        int l = left, m = middle + 1; // 'arr' indexes
 
-        while (leftIndexArr <= middle && middleIndexArr <= right)
-            if (arr[leftIndexArr].CompareTo(arr[middleIndexArr]) < 0)
-                tempArr[leftTemp++] = arr[leftIndexArr++];
-            else
-                tempArr[leftTemp++] = arr[middleIndexArr++];
+        while (l <= middle && m <= right)
+            if (arr[l].CompareTo(arr[m]) < 0) tempArr[i++] = arr[l++];
+            else tempArr[i++] = arr[m++];
 
-        while (leftIndexArr <= middle)
-            tempArr[leftTemp++] = arr[leftIndexArr++];
+        while (l <= middle) tempArr[i++] = arr[l++];
 
-        while (middleIndexArr <= right)
-            tempArr[leftTemp++] = arr[middleIndexArr++];
+        while (m <= right) tempArr[i++] = arr[m++];
 
-        for (int index = left; index <= right; index++)
-            arr[index] = tempArr[index];
+        for (int index = left; index <= right; index++) arr[index] = tempArr[index];
     }
 
-    static void PrintElements<T>(T[] unsortedArr)
-    {
-        foreach (var item in unsortedArr)
-        {
-            Console.Write(item + " ");
-        }
-    }
 }
--- Programming with C#/Codes/Sort/QuickSort.cs
+++ Programming with C#/Codes/Sort/QuickSort.cs
@@ -13,21 +13,21 @@ class QuickSortAlgorithm
     static void TestRunner()
     {
         int[] unsortedNumbers = { 1, -2, 3, -4, 5, -6, 7, -8, 9, -10 };
-        PrintElements(unsortedNumbers); Console.Write(" -> ");
+        Console.Write(string.Join(" ", unsortedNumbers) + " -> ");
         QuickSort(unsortedNumbers, 0, unsortedNumbers.Length - 1);
-        PrintElements(unsortedNumbers); Console.Write("\n");
+        Console.WriteLine(string.Join(" ", unsortedNumbers));
 
         string[] unsortedSymbols = new string[] { "b", "d", "c", "a", "f", "w", "z" };
-        PrintElements(unsortedSymbols); Console.Write(" -> ");
+        Console.Write(string.Join(" ", unsortedSymbols) + " -> ");
         QuickSort(unsortedSymbols, 0, unsortedSymbols.Length - 1);
-        PrintElements(unsortedSymbols); Console.Write("\n");
+        Console.WriteLine(string.Join(" ", unsortedSymbols));
 
         char[] unsortedLetters = { 'z', 'b', 'd', 'c', 'w', 'a', 'f' };
-        PrintElements(unsortedLetters); Console.Write(" -> ");
+        Console.Write(string.Join(" ", unsortedLetters) + " -> ");
         QuickSort(unsortedLetters, 0, unsortedLetters.Length - 1);
-        PrintElements(unsortedLetters); Console.Write("\n");
+        Console.WriteLine(string.Join(" ", unsortedLetters));
     }
-    
+
     static void QuickSort<T>(T[] sortArray, int leftIndex, int rightIndex) where T : IComparable<T>
     {
         /* QUICKSORT(A,p,r)
@@ -50,13 +50,11 @@ class QuickSortAlgorithm
         {
             // 3. Find a larger value to the right of the pivot.
             //    If there is non we end up at the pivot.
-            while (sortArray[leftPointer].CompareTo(frontier) < 0)
-                leftPointer++;
+            while (sortArray[leftPointer].CompareTo(frontier) < 0) leftPointer++;
 
             // 4. Find a smaller value to the left of the pivot.
             //    If there is non we end up at the pivot.
-            while (sortArray[rightPointer].CompareTo(frontier) > 0)
-                rightPointer--;
+            while (sortArray[rightPointer].CompareTo(frontier) > 0) rightPointer--;
 
             // 5. Check if both pointers are not on the pivot.
             if (leftPointer <= rightPointer)
@@ -74,20 +72,12 @@ class QuickSortAlgorithm
 
         // 7. Recursively call the algorithm on the unsorted array
         //    to the left of the pivot (if exists).
-        if (rightPointer > leftIndex)
-            QuickSort(sortArray, leftIndex, rightPointer);
+        if (rightPointer > leftIndex) QuickSort(sortArray, leftIndex, rightPointer);
 
         // 8. Recursively call the algorithm on the unsorted array
         //    to the right of the pivot (if exists).
-        if (leftPointer < rightIndex)
-            QuickSort(sortArray, leftPointer, rightIndex);
+        if (leftPointer < rightIndex) QuickSort(sortArray, leftPointer, rightIndex);
 
         // 9. The algorithm returns when all sub arrays are sorted.
-    }
-
-    static void PrintElements<T>(T[] unsortedArr)
-    {
-        foreach (var item in unsortedArr)
-            Console.Write(item + " ");
     }
 }

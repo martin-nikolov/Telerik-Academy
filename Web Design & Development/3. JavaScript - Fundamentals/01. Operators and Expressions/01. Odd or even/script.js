6a7867f05bc32e5ff026c6ed3c05f8b7e416f4d1
Martin Nikolov <flextry@gmail.com>
2014-01-25 23:41:52 +0200

    Update script.js

--- Web Design & Development/3. JavaScript - Fundamentals/01. Operators and Expressions/01. Odd or even/script.js
+++ Web Design & Development/3. JavaScript - Fundamentals/01. Operators and Expressions/01. Odd or even/script.js
@@ -1,5 +1,5 @@
 /*
-    1. Write an expression that checks if given integer is odd or even.
+	1. Write an expression that checks if given integer is odd or even.
 */
 
 taskName = "1. Odd Or Even";
@@ -8,19 +8,19 @@ function Main(bufferElement) {
 
 	var number = ReadLine("Enter a number: ", GetRandomInt(100));
 
-    SetSolveButton(function () {
-        ConsoleClear();
-    	CheckOddOrEven(number.value);
-    });
+	SetSolveButton(function () {
+		ConsoleClear();
+		CheckOddOrEven(number.value);
+	});
 }
 
 function CheckOddOrEven(number) {
 	number = parseInt(number);
 
-    if (Number.isNaN(number)) {
-        WriteLine("Error! Incorrect input value!");
-        return;
-    }
+	if (Number.isNaN(number)) {
+		WriteLine("Error! Incorrect input value!");
+		return;
+	}
 
 	WriteLine(Format("The number {0} is: {1}", number, (number % 2 != 0 ? "ODD" : "EVEN")));
 }

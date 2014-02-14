6d50cf7c62f2f8bba2cf87a06a0385d372bee55a
flextry <flextry@gmail.com>
2014-02-14 22:29:01 +0200

    JS Part 2 - DOM Manipulation Homework

---
+++ Web Design & Development/4. JavaScript UI/01. DOM Manipulation/03. Query Selector shims/script.js
@@ -0,0 +1,13 @@
+﻿/*
+    3. Write a script that shims querySelector 
+    and querySelectorAll in older browsers
+*/
+
+taskName = "3. Query Selector shims";
+
+function Main(bufferElement) {
+
+    SetSolveButton(function () {
+
+    });
+}

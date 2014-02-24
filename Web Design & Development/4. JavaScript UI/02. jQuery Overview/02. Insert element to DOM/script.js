f5e1eb15a98b77f839f176a5bccba307c8807a4a
flextry <flextry@gmail.com>
2014-02-24 23:04:09 +0200

    JS UI - jQuery Overview Homework Update

---
+++ Web Design & Development/4. JavaScript UI/02. jQuery Overview/02. Insert element to DOM/script.js
@@ -0,0 +1,47 @@
+﻿/*
+    2. Using jQuery implement functionality to insert
+    a DOM element before or after another element
+*/
+
+taskName = "2. Insert element to DOM";
+
+function Main(bufferElement) {
+    var middleElement = createMiddleElement(bufferElement);
+    $(bufferElement).append(middleElement);
+
+    SetSolveButton(function () {
+        InsertModule.insertBefore(middleElement);
+    }, "Insert Before");
+
+    SetSolveButton(function () {
+        InsertModule.insertAfter(middleElement);
+    }, "Insert After");
+}
+
+var InsertModule = (function() {
+    function insertBefore(element) {
+        insertElement(element, 'insertBefore');
+    }
+
+    function insertAfter(element) {
+        insertElement(element, 'insertAfter');
+    }
+
+    function insertElement(element, action) {
+        $('<div class="square inserted" />')[action]($('#middleElement'));
+    }
+
+    return {
+        insertBefore: function(element) { insertBefore(element); }, 
+        insertAfter: function(element) { insertAfter(element); }
+    }
+}());
+
+function createMiddleElement(bufferElement) {
+    var div = $('<div/>');
+    $(div).addClass('square');
+    $(div).attr('id', 'middleElement');
+
+    return div;
+}
+
---
+++ Web Design & Development/4. JavaScript UI/02. jQuery Overview/03. Student table/script.js
@@ -0,0 +1,89 @@
+﻿/*
+    3. By given an array of students, generate a table that represents these students
+    - Each student has first name, last name and grade
+    - Use jQuery
+*/
+
+taskName = "3. Student table";
+
+function Main(bufferElement) {
+    var container = $(_GetDefaultContainer());
+
+    /* ------------------------------------------------------------------------ */
+
+    var students = [
+        new Student("Peter", "Ivanov", 3),
+        new Student("Milena", "Grigorova", 6),
+        new Student("Gergana", "Borisova", 3),
+        new Student("Boyko", "Petrov", 3)
+    ];
+
+    var properties = [ "First name", "Last name", "Grade" ];
+
+    $(container).append(generateTable(students, properties));
+
+    /* ------------------------------------------------------------------------ */
+    
+    var teachers = [
+        new Teacher("Peter", "Ivanov", 35, "Math"),
+        new Teacher("Milena", "Grigorova", 44, "Biology"),
+        new Teacher("Gergana", "Borisova", 28, "Geography"),
+        new Teacher("Boyko", "Petrov", 69, "History")
+    ];
+
+    var properties = [ "First name", "Last name", "Age", "Specialty" ];
+
+    $(container).append(generateTable(teachers, properties));
+
+    /* ------------------------------------------------------------------------ */
+}
+
+function generateTable(collection, properties) {
+    var table = $('<table/>');
+    var thead = $('<thead/>');
+    var tbody = $('<tbody/>');
+
+    // Generate table head
+    var tr = $('<tr/>');
+
+    for (property in properties) {
+        var th = $('<th/>').append(properties[property]);
+        $(tr).append(th);
+    }
+    
+    $(thead).append(tr);
+    $(table).append(thead);
+
+    // Generate table body
+    for (item in collection) {            
+        var tr = $('<tr/>');
+
+        for (property in collection[item]) {
+            var td = $('<td/>').append(collection[item][property]);
+            $(tr).append(td);
+        }
+
+        $(tbody).append(tr);
+    }
+
+    $(table).append(tbody);
+
+    return $(table);
+}
+
+function Student(firstName, lastName, grade) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grade = grade | 0;
+
+    return this;
+}
+
+function Teacher(firstName, lastName, age, specialty) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age | 0;
+    this.specialty = specialty;
+
+    return this;
+}

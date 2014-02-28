7ff02c7e4328c82814469c9c9feb5200691383de
flextry <flextry@gmail.com>
2014-03-01 01:03:13 +0200

    JS OOP - Event Model Update

--- Web Design & Development/5. JavaScript OOP/05. JavaScript Event Model/01. Simple JS Carousel/scripts/script.js
+++ Web Design & Development/5. JavaScript OOP/05. JavaScript Event Model/01. Simple JS Carousel/scripts/script.js
@@ -1,28 +1,93 @@
 window.onload = function() {
-    var step = 913;
+    var _step = 913;
+    var _slides = Math.ceil(document.querySelectorAll('#carousel-list li img').length / 3) | 0;
+    var _currentSlide = 0;
+
+    generateCircles(_slides);
+    colorCircle(_currentSlide);
+    setOnClickToCircles(_step);
+
+    /* -------------------------------------------------------- */
+
     var carouselList = document.getElementById('carousel-list');
 
+    /* -------------------------------------------------------- */
+
+    // Set event to Previous Button
     var previousButton = document.getElementById('previousButton');
 
     previousButton.onclick = function() {
-        carouselList.style.marginLeft = (getPixels(carouselList.style.marginLeft) + step + 'px');
+        carouselList.style.marginLeft = (getPixels(carouselList.style.marginLeft) + _step + 'px');
+
+        _currentSlide--;
 
         if (getPixels(carouselList.style.marginLeft) > 0) {
-            carouselList.style.marginLeft = (-step * 2) + 'px';
+            carouselList.style.marginLeft = (-_step * (_slides - 1)) + 'px';
+
+            _currentSlide = _slides - 1;
         }
+
+        colorCircle(_currentSlide);
     };
 
+    // Set event to Next Button
     var nextButton = document.getElementById('nextButton');
 
     nextButton.onclick = function() {
-        carouselList.style.marginLeft = (getPixels(carouselList.style.marginLeft) - step + 'px');
+        carouselList.style.marginLeft = (getPixels(carouselList.style.marginLeft) - _step + 'px');
 
-        if (getPixels(carouselList.style.marginLeft) <= -step * 3) {
+        _currentSlide++;
+
+        if (getPixels(carouselList.style.marginLeft) <= -_step * _slides) {
             carouselList.style.marginLeft = '0px';
+
+            _currentSlide = 0;
         }
+
+        colorCircle(_currentSlide);
     };
+
+    /* -------------------------------------------------------- */
 
     function getPixels(prop) {
         return parseInt(prop) | 0;
+    }
+
+    function generateCircles(_slidesCount) {
+        var carouselContainer = document.getElementById('carousel-container');
+        var carouselNav = document.createElement('div');
+        carouselNav.id = 'carousel-nav';
+
+        for (var i = 0; i < _slidesCount; i++) {
+            var div = document.createElement('div');
+            carouselNav.appendChild(div);
+        }
+
+        carouselContainer.appendChild(carouselNav);
+    }
+
+    function setOnClickToCircles(step) {
+        var carouselList = document.getElementById('carousel-list');
+        var carouselNav = document.getElementById('carousel-nav');
+
+        for (var i = 0; i < carouselNav.childNodes.length; i++) {
+            carouselNav.childNodes[i].currentIndex = i;
+
+            carouselNav.childNodes[i].addEventListener('click', function() {
+                carouselList.style.marginLeft = (-step * this.currentIndex) + 'px'; 
+                colorCircle(this.currentIndex);
+                _currentSlide = this.currentIndex;
+            });
+        }
+    }
+
+    function colorCircle(slideIndex) {
+        var carouselNav = document.getElementById('carousel-nav');
+        
+        for (var i = 0; i < carouselNav.childNodes.length; i++) {
+            carouselNav.childNodes[i].removeAttribute('class');
+        }
+
+        carouselNav.childNodes[slideIndex].className = "current";   
     }
 }
---
+++ Web Design & Development/5. JavaScript OOP/05. JavaScript Event Model/Readme.md
@@ -0,0 +1,8 @@
+## Events
+
+1. Create a simple JavaScript carousel with `N` images and two arrows for image control
+* Create a TODO list with the following UI controls:
+    * Form input for new Item
+    * Button for adding the new Item
+    * Button for deleting some item
+    * Show and Hide Button

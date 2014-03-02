6d22670969b8cd7a395d834ab5ea078d87766255
flextry <flextry@gmail.com>
2014-03-02 21:11:42 +0200

    CSS Exam Update

---
+++ Web Design & Development/2. CSS Styling/06. CSS - Exam/CSS Styling Exam – Variant 1/Task 2/tab-strip.js
@@ -0,0 +1,16 @@
+﻿(function () {
+	$.fn.tabStrip = function () {
+		var selector = this.selector;
+
+		function onTabBtnClick(e) {
+			$(selector + " .current").removeClass("current");
+			$(this).parent().addClass("current");
+
+			if (e.preventDefault) {
+				e.preventDefault();
+			}
+			return false;
+		}
+		$(selector + " .tab-btn").on("click", onTabBtnClick);
+	};
+})();

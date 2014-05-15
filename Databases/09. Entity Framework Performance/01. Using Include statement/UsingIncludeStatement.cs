8841566b63457a3312fee937da25f1909fead946
Martin Nikolov <flextry@gmail.com>
2014-05-15 23:06:16 +0300

    Database - Homework -U

---
+++ Databases/09. Entity Framework Performance/01. Using Include statement/UsingIncludeStatement.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * 1. Using Entity Framework write a SQL query to select all employees
+ * from the Telerik Academy database and later print their name, 
+ * department and town. Try the both variants: with and without .Include(…).
+ * Compare the number of executed SQL statements and the performance.
+ */
+
+namespace EntityFramework.ConsoleClient
+{
+    using System;
+    using System.Linq;
+    using TelerikAcademy.Models;
+
+    class UsingIncludeStatement
+    {
+        static void Main()
+        {
+            SelectEmployeesUsingIncludeStatement();
+           
+            SelectEmployeedWithoutUsingIncludeStatement();
+        }
+    
+        static void SelectEmployeesUsingIncludeStatement()
+        {
+            using (var dbContext = new TelerikAcademyEntities())
+            {
+                foreach (var employee in dbContext.Employees.Include("Address.Town").Include("Department"))
+                {
+                    Console.WriteLine("{{ Name: {0}, Department: {1}, Town: {2} }}",
+                        employee.FirstName, employee.Department.Name, employee.Address.Town.Name);
+                }
+            }
+        }
+
+        static void SelectEmployeedWithoutUsingIncludeStatement()
+        {
+            using (var dbContext = new TelerikAcademyEntities())
+            {
+                foreach (var employee in dbContext.Employees)
+                {
+                    Console.WriteLine("{{ Name: {0}, Department: {1}, Town: {2} }}",
+                        employee.FirstName, employee.Department.Name, employee.Address.Town.Name);
+                }
+            }
+        }
+    }
+}
---
+++ Databases/09. Entity Framework Performance/02. Using ToList each time/UsingToListEachTime.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * 2. Using Entity Framework write a query that selects all employees 
+ * from the Telerik Academy database, then invokes ToList(), then selects
+ * their addresses, then invokes ToList(), then selects their towns,
+ * then invokes ToList() and finally checks whether the town is "Sofia".
+ * Rewrite the same in more optimized way and compare the performance.
+ */
+
+namespace EntityFramework.ConsoleClient
+{
+    using System;
+    using System.Linq;
+    using TelerikAcademy.Models;
+
+    class UsingToListEachTime
+    {
+        static void Main()
+        {
+            UsingToListEachTimeTest();
+
+            UsingToListOnceTest();
+        }
+
+        static void UsingToListEachTimeTest()
+        {
+            using (var dbContext = new TelerikAcademyEntities())
+            {
+                var employees = dbContext.Employees
+                                         .ToList()
+                                         .Select(e => e.Address)
+                                         .ToList()
+                                         .Select(a => a.Town)
+                                         .ToList()
+                                         .Where(t => t.Name == "Sofia")
+                                         .ToList();
+
+                Console.WriteLine("Records: " + employees.Count);
+            }
+        }
+
+        static void UsingToListOnceTest()
+        {
+            using (var dbContext = new TelerikAcademyEntities())
+            {
+                var employees = dbContext.Employees
+                                         .Select(e => e.Address)
+                                         .Select(a => a.Town)
+                                         .Where(t => t.Name == "Sofia")
+                                         .ToList();
+
+                Console.WriteLine("Records: " + employees.Count);
+            }
+        }
+    }
+}

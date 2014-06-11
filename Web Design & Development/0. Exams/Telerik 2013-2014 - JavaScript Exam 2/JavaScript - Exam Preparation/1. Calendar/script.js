a7b5c60e62bb50b50c64963b041e6e83f0d7e624
Martin Nikolov <flextry@gmail.com>
2014-06-11 19:14:22 +0300

    Update

---
+++ Web Design & Development/0. Exams/Telerik 2013-2014 - JavaScript Exam 2/JavaScript - Exam Preparation/1. Calendar/script.js
@@ -0,0 +1,105 @@
+var DAYS_OF_WEEK = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+var MONTH = 'June';
+var YEAR = '2014';
+var MAX_DAYS = 30;
+
+function createCalendar(selector, events) {
+    var table = createTable(events);
+    setStylesAndEvents(table);
+    
+    var container = document.querySelector(selector);
+    container.appendChild(table);
+}
+
+function createTable(events) {
+    var table = document.createElement('table');
+    var tr = document.createElement('tr');
+
+    for (var currentDay = 1; currentDay <= MAX_DAYS; currentDay++) {
+        var titleDiv = document.createElement('div');
+        titleDiv.className = 'day-title';
+        titleDiv.innerHTML = titleToString(currentDay);
+
+        var eventsContainer = document.createElement('div');
+        eventsContainer.className = 'day-events';
+        for (var j = 0; j < events.length; j++) {
+            if ((events[j].date | 0) === currentDay) {
+                eventsContainer.innerHTML = eventToString(events[j]);
+                break;
+            }
+        }
+
+        var td = document.createElement('td');
+        td.className = 'day-col';
+        td.appendChild(titleDiv);
+        td.appendChild(eventsContainer);
+
+        tr.appendChild(td);
+
+        if (currentDay % 7 === 0) {
+            table.appendChild(tr);
+            tr = document.createElement('tr');
+        }
+    }
+
+    table.appendChild(tr);
+    return table;
+}
+
+function setStylesAndEvents(table) {
+    table.style.borderCollapse = 'collapse';
+
+    var allTds = table.getElementsByClassName('day-col');
+    for (var i = 0; i < allTds.length; i++) {
+        allTds[i].style.width = '125px';
+        allTds[i].style.height = '105px';
+        allTds[i].style.padding = '0';
+        allTds[i].style.border = '2px solid black';
+    }
+
+    var allTitles = table.getElementsByClassName('day-title');
+    for (var i = 0; i < allTitles.length; i++) {
+        allTitles[i].style.marginTop = '-52px';
+        allTitles[i].style.textAlign = 'center';
+        allTitles[i].style.background = '#CCCCCC';
+        allTitles[i].style.borderBottom = '1px solid black';
+
+        allTitles[i].addEventListener('click', onClick, false);
+        allTitles[i].addEventListener('mouseover', onMouseOver, false);
+        allTitles[i].addEventListener('mouseout', onMouseOut, false);
+    }
+}
+
+function onClick(e) {
+    e.target.style.background = '#FFFFFF';
+
+    if (e.target.className.indexOf('day-title') != -1) {
+        var lastCurrent = document.getElementById('current');
+        if (lastCurrent) {
+            lastCurrent.removeAttribute('id');
+            lastCurrent.addEventListener('mouseover', onMouseOver, false);
+            lastCurrent.addEventListener('mouseout', onMouseOut, false);
+            onMouseOut(lastCurrent);
+        }
+
+        e.target.setAttribute('id', 'current');
+        e.target.removeEventListener('mouseover', onMouseOver, false);
+        e.target.removeEventListener('mouseout', onMouseOut, false);
+    }
+}
+
+function onMouseOver(e) {
+    (e.target || e).style.background = '#999999';
+}
+
+function onMouseOut(e) {
+    (e.target || e).style.background = '#CCCCCC';
+}
+
+function titleToString(currentDay) {
+    return DAYS_OF_WEEK[(currentDay - 1) % 7] + ' ' + currentDay + ' ' + MONTH + ' ' + YEAR;
+}
+
+function eventToString(eventData) {
+    return eventData.hour + ' ' + eventData.title;
+}

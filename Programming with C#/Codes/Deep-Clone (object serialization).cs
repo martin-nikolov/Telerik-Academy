4c182eb40952dccd267b7495a0eac64df6645129
Martin Nikolov <flextry@gmail.com>
2014-07-05 16:31:25 +0300

    C# Codes Deep-Clone (object serialization) algorithm added

---
+++ Programming with C#/Codes/Deep-Clone (object serialization).cs
@@ -0,0 +1,14 @@
+public static class ExtensionMethods
+{
+    // Deep clone
+    public static T DeepClone<T>(this T a)
+    {
+        using (MemoryStream stream = new MemoryStream())
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, a);
+            stream.Position = 0;
+            return (T) formatter.Deserialize(stream);
+        }
+    }
+}

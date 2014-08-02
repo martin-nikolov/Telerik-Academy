b0482309b6b2509eb456f46c8121d61d906d7c95
Martin Nikolov <flextry@gmail.com>
2014-08-02 22:14:52 +0300

    Add new Logger Diagram updated

--- Programming with C#/0. Exams/Telerik 2013-2014 - High-Quality Code/C# High-Quality Code 2014 - Sample Exam/Phonebook.ConsoleClient/PhonebookEntryPoint.cs
+++ Programming with C#/0. Exams/Telerik 2013-2014 - High-Quality Code/C# High-Quality Code 2014 - Sample Exam/Phonebook.ConsoleClient/PhonebookEntryPoint.cs
@@ -29,7 +29,7 @@
             }
 
             //! Visitor
-            consoleLogger.Accept(new ConsoleLoggerVisitorWithoutNewLine());
+            consoleLogger.Accept(new ConsoleLoggerVisitorWithNewLine());
         }
     }
 }
---
+++ Programming with C#/0. Exams/Telerik 2013-2014 - High-Quality Code/C# High-Quality Code 2014 - Sample Exam/Phonebook.Data/Loggers/ListOfStringsLogger.cs
@@ -0,0 +1,24 @@
+﻿namespace Phonebook.Data.Loggers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Phonebook.Data.Contracts;
+
+    //! Strategy pattern
+    public class ListOfStringsLogger : ILogger
+    {
+        private readonly IList<string> output = new List<string>();
+
+        public void AppendLine(string text)
+        {
+            this.output.Add(text);
+        }
+
+        public void Accept(ILoggerVisitor visitor)
+        {
+            var mergedLinesString = string.Join(Environment.NewLine, this.output);
+            visitor.Visit(mergedLinesString);
+        }
+    }
+}

92a52fe3b4f469c6642c9ab24ff4928cc17237cc
Martin Nikolov <flextry@gmail.com>
2014-08-03 19:52:25 +0300

    Add excluded files

---
+++ Programming with C#/0. Exams/Telerik 2013-2014 - High-Quality Code/C# High-Quality Code 2014 - Sample Exam/Phonebook.Tests/PhoneRepositoryTests/ChangePhoneTest.cs
@@ -0,0 +1,88 @@
+﻿namespace Phonebook.Tests.PhoneRepositoryTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Phonebook.Data.Repositories;
+
+    [TestClass]
+    public class ChangePhoneTest
+    {
+        [TestMethod]
+        public void ChangeExistingPhoneNumberShouldRemoveOldAndAddNewPhoneNumber()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.AddPhone("John", new List<string>() { "111", "222" });
+            var matchedPhoneEntries = phonebookRepository.ChangePhone("111", "333");
+
+            Assert.AreEqual(1, matchedPhoneEntries);
+            Assert.AreEqual(2, phonebookRepository.ListEntries(0, 1).First().PhoneNumbers.Count);
+            Assert.IsTrue(phonebookRepository.ListEntries(0, 1).First().PhoneNumbers.Contains("333"));
+            Assert.IsFalse(phonebookRepository.ListEntries(0, 1).First().PhoneNumbers.Contains("111"));
+        }
+
+        [TestMethod]
+        public void ChangeNonExistingPhoneNumberShouldNotAffectToTheRepository()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.AddPhone("John", new List<string>() { "111", "222" });
+            var matchedPhoneEntries = phonebookRepository.ChangePhone("333", "444");
+
+            Assert.AreEqual(0, matchedPhoneEntries);
+            Assert.AreEqual(2, phonebookRepository.ListEntries(0, 1).First().PhoneNumbers.Count);
+            CollectionAssert.AreEqual(new List<string>() { "111", "222" }, phonebookRepository.ListEntries(0, 1).First().PhoneNumbers.ToList());
+        }
+
+        [TestMethod]
+        public void ChangedSharedPhoneNumber()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.AddPhone("John", new List<string>() { "111", "222" });
+            phonebookRepository.AddPhone("Smith", new List<string>() { "222", "333" });
+            var matchedPhoneEntries = phonebookRepository.ChangePhone("222", "444");
+
+            Assert.AreEqual(2, matchedPhoneEntries);
+            Assert.AreEqual(2, phonebookRepository.ListEntries(0, 2).First().PhoneNumbers.Count);
+            Assert.AreEqual(2, phonebookRepository.ListEntries(0, 2).Skip(1).First().PhoneNumbers.Count);
+
+            Assert.IsTrue(phonebookRepository.ListEntries(0, 2).First().PhoneNumbers.Contains("444"));
+            Assert.IsFalse(phonebookRepository.ListEntries(0, 2).First().PhoneNumbers.Contains("222"));
+
+            Assert.IsTrue(phonebookRepository.ListEntries(0, 2).Skip(1).First().PhoneNumbers.Contains("444"));
+            Assert.IsFalse(phonebookRepository.ListEntries(0, 2).Skip(1).First().PhoneNumbers.Contains("222"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ChangePhoneOnNullOldPhoneNumberShouldThrowException()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.ChangePhone(null, "111");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ChangePhoneOnEmptyOldPhoneNumberShouldThrowException()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.ChangePhone(null, "111");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ChangePhoneOnNullNewPhoneNumberShouldThrowException()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.ChangePhone("", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ChangePhoneOnEmptyNewPhoneNumberShouldThrowException()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.ChangePhone("", null);
+        }
+    }
+}
---
+++ Programming with C#/0. Exams/Telerik 2013-2014 - High-Quality Code/C# High-Quality Code 2014 - Sample Exam/Phonebook.Tests/PhoneRepositoryTests/ListPhoneTest.cs
@@ -0,0 +1,73 @@
+﻿namespace Phonebook.Tests.PhoneRepositoryTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Phonebook.Data.Repositories;
+
+    [TestClass]
+    public class ListPhoneTest
+    {
+        [TestMethod]
+        public void ListSingleEntry()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.AddPhone("John", new List<string>() { "111", "222" });
+            var result = phonebookRepository.ListEntries(0, 1);
+            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual("John", result.First().Name);
+            Assert.AreEqual(2, result.First().PhoneNumbers.Count);
+            CollectionAssert.AreEqual(new List<string>() { "111", "222" }, result.First().PhoneNumbers.ToList());
+        }
+
+        [TestMethod]
+        public void ListMultipleEntry()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.AddPhone("John", new List<string>() { "111", "222" });
+            phonebookRepository.AddPhone("John1", new List<string>() { "333", "444", "4444" });
+            phonebookRepository.AddPhone("John2", new List<string>() { "555", "666" });
+            var result = phonebookRepository.ListEntries(0, 3);
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual("John", result.First().Name);
+            Assert.AreEqual(2, result.First().PhoneNumbers.Count);
+            CollectionAssert.AreEqual(new List<string>() { "333", "444", "4444" }, result.Skip(1).First().PhoneNumbers.ToList());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ListEntriesWithInvalidIndexShouldThrowException()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.AddPhone("John", new List<string>() { "111", "222" });
+            phonebookRepository.ListEntries(-1, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ListEntriesWithNegativeCountShouldThrowException()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.AddPhone("John", new List<string>() { "111", "222" });
+            phonebookRepository.ListEntries(0, -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ListEntriesWithCountBiggetThanMaxCountShouldThrowException()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.AddPhone("John", new List<string>() { "111", "222" });
+            phonebookRepository.ListEntries(0, int.MaxValue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ListEntriesWithInvalidIndexAndCountShouldThrowException()
+        {
+            var phonebookRepository = new PhonebookRepositoryFast();
+            phonebookRepository.ListEntries(1, 1);
+        }
+    }
+}

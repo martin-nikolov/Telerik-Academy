b43719f725542b259bd3bd12ef4892b42fb6d93f
Martin Nikolov <flextry@gmail.com>
2014-08-29 14:27:22 +0300

    EF -HW -U

--- Databases/08. Entity Framework/08. Extend Employee class/EmployeeExtendedTest.cs
+++ Databases/08. Entity Framework/08. Extend Employee class/EmployeeExtendedTest.cs
@@ -8,12 +8,23 @@ namespace EntityFramework.ConsoleClient
 {
     using System;
     using System.Linq;
+    using Northwind.Models;
 
     public class EmployeeExtendedTest
     {
         public static void Main()
         {
-            // EntitySet<T> is not available in EF 5/6 versions.
+            // See file -> Northwind.Models -> `Employee.cs
+ 
+            using (var dbContext = new NorthwindEntities())
+            {
+                foreach (var employee in dbContext.Employees.Include("Territories"))
+                {
+                    var correspondingTerritories = employee.CorrespondingTerritories.Select(c => c.TerritoryID);
+                    var correspondingTerritoriesAsString = string.Join(", ", correspondingTerritories);
+                    Console.WriteLine("{0} -> Territory IDs: {1}", employee.FirstName, correspondingTerritoriesAsString);
+                }
+            }
         }
     }
 }
---
+++ Databases/08. Entity Framework/Northwind.Models/`Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace Northwind.Models
+{
+    using System;
+    using System.Data.Linq;
+    using System.Linq;
+
+    public partial class Employee
+    {
+        public EntitySet<Territory> CorrespondingTerritories
+        {
+            get
+            {
+                var territoriesAsEntitySet = new EntitySet<Territory>();
+                territoriesAsEntitySet.AddRange(this.Territories);
+                return territoriesAsEntitySet;
+            }
+        }
+    }
+}

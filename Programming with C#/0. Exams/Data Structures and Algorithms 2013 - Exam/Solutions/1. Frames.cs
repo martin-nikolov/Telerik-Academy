809aaadc2a3a0dadf1eb5824a4ac8b816a3acc27
Martin Nikolov <flextry@gmail.com>
2014-09-12 23:35:55 +0300

    Update

--- Programming with C#/0. Exams/Data Structures and Algorithms 2013 - Exam/Solutions/1. Frames.cs
+++ Programming with C#/0. Exams/Data Structures and Algorithms 2013 - Exam/Solutions/1. Frames.cs
@@ -1,126 +1,126 @@
-namespace DataStructuresAndAlgorithms
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Frames
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
+    private const int MaximalFramesCount = 6;
+    private static readonly bool[] used = new bool[MaximalFramesCount];
+    private static readonly int[] permutations = new int[MaximalFramesCount];
 
-    public class Frames
+    private static int numberOfFrames;
+    private static readonly Frame[] frames = new Frame[MaximalFramesCount];
+    private static readonly ISet<string> uniqueFrames = new HashSet<string>();
+    private static readonly StringBuilder output = new StringBuilder();
+
+    internal static void Main()
     {
-        private const int MaximalFramesCount = 6;
-        private static readonly bool[] used = new bool[MaximalFramesCount];
-        private static readonly int[] permutations = new int[MaximalFramesCount];
+        #if DEBUG
+            Console.SetIn(new System.IO.StreamReader("../../input.txt"));
+        #endif
 
-        private static int numberOfFrames;
-        private static readonly Frame[] frames = new Frame[MaximalFramesCount];
-        private static readonly ISet<string> uniqueFrames = new SortedSet<string>();
+        ParseInput();
+        PermutationsWithoutReps();
+        PrintAnswer();
+    }
 
-        internal static void Main()
+    private static void ParseInput()
+    {
+        numberOfFrames = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfFrames; i++)
         {
-            #if DEBUG
-                Console.SetIn(new System.IO.StreamReader("../../input.txt"));
-            #endif
-
-            ParseInput();
-            PermutationsWithoutReps();
-            PrintAnswer();
-        }
- 
-        private static void ParseInput()
-        {
-            numberOfFrames = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < numberOfFrames; i++)
-            {
-                var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-                frames[i] = new Frame(numbers[0], numbers[1]);
-            }
-        }
-
-        private static void PermutationsWithoutReps(int index = 0)
-        {
-            if (index >= numberOfFrames)
-            {
-                var currentFrames = GetFramePermutation();
-                SaveFramePermutation(currentFrames);
-                FlipFrames(currentFrames, 0);
-                return;
-            }
-
-            for (int i = 0; i < numberOfFrames; i++)
-            {
-                if (used[i])
-                {
-                    continue;
-                }
-
-                used[i] = true;
-                permutations[index] = i;
-                PermutationsWithoutReps(index + 1);
-                used[i] = false;
-            }
-        }
-
-        private static void FlipFrames(Frame[] framesToFlip, int index)
-        {
-            if (index == numberOfFrames)
-            {
-                return;
-            }
-
-            // Swap values
-            framesToFlip[index] = new Frame(framesToFlip[index].SecondValue, framesToFlip[index].FirstValue);
-            FlipFrames(framesToFlip, index + 1);
-
-            SaveFramePermutation(framesToFlip);
-
-            // Swap values again - recover to original frame
-            framesToFlip[index] = new Frame(framesToFlip[index].SecondValue, framesToFlip[index].FirstValue);
-            FlipFrames(framesToFlip, index + 1);
-        }
-
-        private static void SaveFramePermutation(Frame[] framesToFlip)
-        {
-            uniqueFrames.Add(string.Join(" | ", framesToFlip));
-        }
-
-        private static Frame[] GetFramePermutation()
-        {
-            var currentFrames = new Frame[numberOfFrames];
-
-            for (int i = 0; i < numberOfFrames; i++)
-            {
-                currentFrames[i] = frames[permutations[i]];
-            }
-
-            return currentFrames;
-        }
-
-        private static void PrintAnswer()
-        {
-            Console.WriteLine(uniqueFrames.Count);
-            foreach (var frame in uniqueFrames)
-            {
-                Console.WriteLine(frame);
-            }
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            frames[i] = new Frame(numbers[0], numbers[1]);
         }
     }
 
-    public struct Frame
+    private static void PermutationsWithoutReps(int index = 0)
     {
-        public Frame(int firstValue, int secondValue)
-            : this()
+        if (index >= numberOfFrames)
         {
-            this.FirstValue = firstValue;
-            this.SecondValue = secondValue;
+            var currentFrames = GetFramePermutation();
+            SaveFramePermutation(currentFrames);
+            FlipFrames(currentFrames, 0);
+            return;
         }
 
-        public int FirstValue { get; set; }
-
-        public int SecondValue { get; set; }
-
-        public override string ToString()
+        for (int i = 0; i < numberOfFrames; i++)
         {
-            return string.Format("({0}, {1})", this.FirstValue, this.SecondValue);
+            if (used[i])
+            {
+                continue;
+            }
+
+            used[i] = true;
+            permutations[index] = i;
+            PermutationsWithoutReps(index + 1);
+            used[i] = false;
         }
+    }
+
+    private static void FlipFrames(Frame[] framesToFlip, int index)
+    {
+        if (index == numberOfFrames)
+        {
+            return;
+        }
+
+        // Swap values
+        framesToFlip[index] = new Frame(framesToFlip[index].SecondValue, framesToFlip[index].FirstValue);
+        FlipFrames(framesToFlip, index + 1);
+
+        SaveFramePermutation(framesToFlip);
+
+        // Swap values again - recover to original frame
+        framesToFlip[index] = new Frame(framesToFlip[index].SecondValue, framesToFlip[index].FirstValue);
+        FlipFrames(framesToFlip, index + 1);
+    }
+
+    private static void SaveFramePermutation(Frame[] framesToFlip)
+    {
+        uniqueFrames.Add(string.Join(" | ", framesToFlip));
+    }
+
+    private static Frame[] GetFramePermutation()
+    {
+        var currentFrames = new Frame[numberOfFrames];
+
+        for (int i = 0; i < numberOfFrames; i++)
+        {
+            currentFrames[i] = frames[permutations[i]];
+        }
+
+        return currentFrames;
+    }
+
+    private static void PrintAnswer()
+    {
+        Console.WriteLine(uniqueFrames.Count);
+        foreach (var frame in uniqueFrames.OrderBy(a => a))
+        {
+            output.AppendLine(frame);
+        }
+        Console.WriteLine(output.ToString().Trim());
+    }
+}
+
+public struct Frame
+{
+    public Frame(int firstValue, int secondValue)
+        : this()
+    {
+        this.FirstValue = firstValue;
+        this.SecondValue = secondValue;
+    }
+
+    public int FirstValue { get; set; }
+
+    public int SecondValue { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("({0}, {1})", this.FirstValue, this.SecondValue);
     }
 }
--- Programming with C#/0. Exams/Data Structures and Algorithms 2013 - Exam/Solutions/5. Businessmen (Catalan - Factorial).cs
+++ Programming with C#/0. Exams/Data Structures and Algorithms 2013 - Exam/Solutions/5. Businessmen (Catalan - Factorial).cs
@@ -2,13 +2,16 @@ using System;
 using System.Linq;
 using System.Numerics;
 
+/// <summary>
+/// Formula: Factorial(2 * N) / [Factorial(N + 1) * Factorial(N)]
+/// </summary>
 class Businessmen
 {
     static void Main()
     {
         int N = int.Parse(Console.ReadLine()) / 2;
 
-        Console.WriteLine(Factorial(2 * N) / (Factorial(N + 1) * Factorial(N))); // Slow
+        Console.WriteLine(Factorial(2 * N) / (Factorial(N + 1) * Factorial(N)));
     }
 
     static BigInteger Factorial(int n)

b80b8d07d598e05dd050c45cc50922ab82d77f8a
Martin Nikolov <flextry@gmail.com>
2014-09-20 00:41:15 +0300

    Update

--- Web Services and Cloud Technologies/04. Windows Communication Foundation (WCF)/02. SubstringModule.Web/Default.aspx.cs
+++ Web Services and Cloud Technologies/04. Windows Communication Foundation (WCF)/02. SubstringModule.Web/Default.aspx.cs
@@ -1,12 +1,48 @@
 ﻿namespace SubstringModule.Web
 {
     using System;
+    using System.ServiceModel;
+    using System.ServiceModel.Description;
     using System.Web.UI;
+    using SubstringModule.Services;
     using SubstringModule.Web.ServiceSubstringModule;
 
     public partial class Default : Page
     {
+        private const string HostUrl = "http://localhost:8733/Design_Time_Addresses/SubstringModule.Services/";
+
         protected void OnGetNumberOfSubstringButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                // Try to open host -> if host is already open it's throws an exception
+                using (var selfHost = HostSubstringModuleService())
+                {
+                    selfHost.Open();
+                    this.GetNumberOfSubstrings();
+                }
+            }
+            catch (Exception)
+            {
+                // Use already opened host
+                this.GetNumberOfSubstrings();
+            }
+        }
+ 
+        private static ServiceHost HostSubstringModuleService()
+        {
+            var serviceAddress = new Uri(HostUrl);
+            var smb = new ServiceMetadataBehavior()
+            {
+                HttpGetEnabled = true
+            };
+
+            var selfHost = new ServiceHost(typeof(SubstringService), serviceAddress);
+            selfHost.Description.Behaviors.Add(smb);
+            return selfHost;
+        }
+
+        private void GetNumberOfSubstrings()
         {
             var client = new SubstringServiceClient();
 
